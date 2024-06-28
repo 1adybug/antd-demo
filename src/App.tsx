a8b5d646c12f51edc72d5717536dbf887ecf3681
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-06-28 11:07:25 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { FC } from "react"
 
 const App: FC = () => {
     return (
-<div style={{ width: 400 }}>
-    <div>
-        <Input allowClear />
-    </div>
-    <div>
-        <Input.TextArea allowClear />
-    </div>
-</div>
+        <div style={{ width: 400 }}>
+            <div>
+                <Input allowClear />
+            </div>
+            <div>
+                <Input.TextArea allowClear />
+            </div>
+        </div>
     )
 }
 
